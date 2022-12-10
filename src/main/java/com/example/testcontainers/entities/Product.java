9999717882e717c99933eafff522efd85fe5fd44
Ale <alejandrohierro@gmail.com>
2022-12-10 11:35:00 -0300

    first commit

---
+++ src/main/java/com/example/testcontainers/entities/Product.java
@@ -0,0 +1,24 @@
+package com.example.testcontainers.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "PRODUCTS")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
